aaf9e0089c439dda907e32da43259a3b1dfa66cb
Evgeniy Lapin <lev@openteam.ru>
2017-09-04 10:59:43 +0700

    squish video code

--- app/helpers/video_helper.rb
+++ app/helpers/video_helper.rb
@@ -1,6 +1,6 @@
 module VideoHelper
   def youtube_object(code)
-    code = strip_tags(code)
+    code = strip_tags(code).squish
     youtube_link = "http://www.youtube.com/watch?v=#{code}"
 
     VideoInfo.new(youtube_link)
